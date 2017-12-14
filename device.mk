05b690bb0d6d0e92c2fab97dc9dd9b1621a3d586
Ded_Boi <dhanush9111gtr@gmail.com>
2017-12-14 16:37:07 +0000

    Revert "op3: Add OnePlus Camera and blobs (OOS 4.1.6)"
    This reverts commit 4c0616dab9a9357767c3697a6f3b3df280e9088a.

--- device.mk
+++ device.mk
@@ -159,8 +159,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libcamera_shim \
-    OnePlusCamera
+    libcamera_shim
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
